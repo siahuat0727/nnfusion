63590946156166430b3ae6e678acfabafc82f956
siahuat0727 <tansiahuat@gmail.com>
2021-10-26 06:06:28 +0000

    Update Ubuntu 16.04 dependency

--- maint/dockerfile/nnfusion_ubuntu_18.04.dockerfile
+++ maint/dockerfile/nnfusion_ubuntu_18.04.dockerfile
@@ -1,14 +1,17 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-FROM ubuntu:18.04
+FROM nvidia/cuda:11.3.1-cudnn8-devel-ubuntu16.04
 RUN apt update && apt install -y git
-RUN git clone https://github.com/microsoft/nnfusion.git /root/nnfusion --branch master --single-branch
-# - Install Requirements 
+# FIXME
+# RUN git clone https://github.com/microsoft/nnfusion.git /root/nnfusion --branch master --single-branch
+RUN git clone https://github.com/siahuat0727/nnfusion.git /root/nnfusion --branch siahuat0727/update-dependency --single-branch
+# - Install Requirements
 RUN bash /root/nnfusion/maint/script/install_dependency.sh
 # - Make Install
 RUN cd /root/nnfusion/ && mkdir build && cd build && cmake .. && make -j6 && make install
 # - Execute command
 RUN LD_LIBRARY_PATH=/usr/local/lib nnfusion /root/nnfusion/test/models/tensorflow/frozen_op_graph/frozen_abs_graph.pb
-RUN apt install -y python3 python3-pip
-RUN pip3 install numpy
+# FIXME remove
+RUN wget https://nnfusion.blob.core.windows.net/models/tensorflow/frozen_lstm_l8s8h256_bs1.pb
+RUN LD_LIBRARY_PATH=/usr/local/lib nnfusion frozen_lstm_l8s8h256_bs1.pb
--- maint/script/install_dependency.sh
+++ maint/script/install_dependency.sh
@@ -17,50 +17,29 @@ ubuntu_codename=$(. /etc/os-release;echo $UBUNTU_CODENAME)
 if ! dpkg -L $DEB_PACKAGES >/dev/null 2>&1; then
 	#Thirdparty deb for ubuntu 18.04(bionic)
 	$SUDO sh -c "apt update && apt install -y --no-install-recommends software-properties-common apt-transport-https ca-certificates gnupg wget"
-    $SUDO sh -c "wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null"
-    $SUDO sh -c "apt-add-repository 'deb https://apt.kitware.com/ubuntu/ $ubuntu_codename main'"
-	$SUDO sh -c "add-apt-repository ppa:maarten-fonville/protobuf -y" 
+	$SUDO sh -c "wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null"
+	$SUDO sh -c "apt-add-repository 'deb https://apt.kitware.com/ubuntu/ $ubuntu_codename main'"
 	$SUDO sh -c "apt update && apt install -y --no-install-recommends $DEB_PACKAGES"
 fi
 echo "- Dependencies are installed in system."
-    #Install latest CMake
+
 
 # if Ubuntu 16.04, we have some dev node using ubuntu 16.04
 if [[ $ubuntu_codename == "xenial" ]]; then
-    echo "- Ubuntu 16.04 detected. Download & install latest protobuf & gtest debs."
+    echo "- Ubuntu 16.04 detected. Download & install protobuf 3.6.1 & gtest debs."
 
-    # Install protobuf & gtest from bionic version
-    $SUDO sh -c " \
-    wget https://launchpad.net/~maarten-fonville/+archive/ubuntu/protobuf/+files/libprotobuf17_3.6.1-1~maarten0+bionic_amd64.deb -P /tmp && \
-    wget https://launchpad.net/~maarten-fonville/+archive/ubuntu/protobuf/+files/libprotoc17_3.6.1-1~maarten0+bionic_amd64.deb -P /tmp && \
-    wget https://launchpad.net/~maarten-fonville/+archive/ubuntu/protobuf/+files/libprotoc-dev_3.6.1-1~maarten0+bionic_amd64.deb -P /tmp && \
-    wget https://launchpad.net/~maarten-fonville/+archive/ubuntu/protobuf/+files/libprotobuf-lite17_3.6.1-1~maarten0+bionic_amd64.deb -P /tmp && \
-    wget https://launchpad.net/~maarten-fonville/+archive/ubuntu/protobuf/+files/libprotobuf-dev_3.6.1-1~maarten0+bionic_amd64.deb -P /tmp && \
-    wget https://launchpad.net/~maarten-fonville/+archive/ubuntu/protobuf/+files/protobuf-compiler_3.6.1-1~maarten0+bionic_amd64.deb -P /tmp && \
-    wget https://launchpad.net/~maarten-fonville/+archive/ubuntu/protobuf/+files/googletest_1.9.0.20190831-2~202001251824~ubuntu18.04.1_all.deb -P /tmp && \
-    wget https://launchpad.net/~maarten-fonville/+archive/ubuntu/protobuf/+files/libgtest-dev_1.9.0.20190831-2~202001251824~ubuntu18.04.1_amd64.deb -P /tmp && \
-    wget https://launchpad.net/~maarten-fonville/+archive/ubuntu/protobuf/+files/google-mock_1.9.0.20190831-2~202001251824~ubuntu18.04.1_amd64.deb -P /tmp &&\
-    wget https://launchpad.net/~maarten-fonville/+archive/ubuntu/protobuf/+files/googletest-tools_1.9.0.20190831-2~202001251824~ubuntu18.04.1_amd64.deb -P /tmp &&\
-    wget https://launchpad.net/~maarten-fonville/+archive/ubuntu/protobuf/+files/libgmock-dev_1.9.0.20190831-2~202001251824~ubuntu18.04.1_amd64.deb -P /tmp\
-    "
+    # Install protobuf 3.6.1
+    $SUDO sh -c "wget https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protobuf-cpp-3.6.1.tar.gz -P /tmp"
+    $SUDO sh -c "cd /tmp && tar -xf /tmp/protobuf-cpp-3.6.1.tar.gz && rm /tmp/protobuf-cpp-3.6.1.tar.gz"
+    $SUDO sh -c "cd /tmp/protobuf-3.6.1/ && ./configure && make && make check && make install && ldconfig && rm -rf /tmp/protobuf-3.6.1/"
 
-    $SUDO sh -c "dpkg -i /tmp/libprotobuf17_3.6.1-1~maarten0+bionic_amd64.deb /tmp/libprotoc17_3.6.1-1~maarten0+bionic_amd64.deb \
-    /tmp/libprotoc-dev_3.6.1-1~maarten0+bionic_amd64.deb /tmp/googletest_1.9.0.20190831-2~202001251824~ubuntu18.04.1_all.deb \
-    /tmp/libprotobuf-lite17_3.6.1-1~maarten0+bionic_amd64.deb /tmp/libprotobuf-dev_3.6.1-1~maarten0+bionic_amd64.deb \
-    /tmp/protobuf-compiler_3.6.1-1~maarten0+bionic_amd64.deb /tmp/libgtest-dev_1.9.0.20190831-2~202001251824~ubuntu18.04.1_amd64.deb \
-    /tmp/google-mock_1.9.0.20190831-2~202001251824~ubuntu18.04.1_amd64.deb /tmp/googletest-tools_1.9.0.20190831-2~202001251824~ubuntu18.04.1_amd64.deb \
-    /tmp/libgmock-dev_1.9.0.20190831-2~202001251824~ubuntu18.04.1_amd64.deb"
-
-    $SUDO sh -c "rm /tmp/libprotobuf17_3.6.1-1~maarten0+bionic_amd64.deb /tmp/libprotoc17_3.6.1-1~maarten0+bionic_amd64.deb \
-    /tmp/libprotoc-dev_3.6.1-1~maarten0+bionic_amd64.deb /tmp/googletest_1.9.0.20190831-2~202001251824~ubuntu18.04.1_all.deb \
-    /tmp/libprotobuf-lite17_3.6.1-1~maarten0+bionic_amd64.deb /tmp/libprotobuf-dev_3.6.1-1~maarten0+bionic_amd64.deb \
-    /tmp/protobuf-compiler_3.6.1-1~maarten0+bionic_amd64.deb /tmp/libgtest-dev_1.9.0.20190831-2~202001251824~ubuntu18.04.1_amd64.deb \
-    /tmp/google-mock_1.9.0.20190831-2~202001251824~ubuntu18.04.1_amd64.deb /tmp/googletest-tools_1.9.0.20190831-2~202001251824~ubuntu18.04.1_amd64.deb \
-    /tmp/libgmock-dev_1.9.0.20190831-2~202001251824~ubuntu18.04.1_amd64.deb"
-
+    # Compile gtest
+    $SUDO sh -c "cd /usr/src/gtest && mkdir -p  build && cd build && cmake .. -DCMAKE_CXX_FLAGS=\"-std=c++11\" && make -j"
+    $SUDO sh -c "cp /usr/src/gtest/build/libgtest*.a /usr/lib/"
+    $SUDO sh -c "rm -rf /usr/src/gtest/build"
 fi
 
-if [ ! -f "/usr/lib/libgtest.a" ]; then 
+if [ ! -f "/usr/lib/libgtest.a" ]; then
     $SUDO sh -c "cd /usr/src/googletest/googletest && mkdir -p build && cd build && cmake .. -DCMAKE_CXX_FLAGS=\"-std=c++11\" && make -j"
     $SUDO sh -c "cp /usr/src/googletest/googletest/build/lib/libgtest* /usr/lib/"
     $SUDO sh -c "rm -rf /usr/src/googletest/googletest/build"
@@ -69,8 +48,5 @@ if [ ! -f "/usr/lib/libgtest.a" ]; then
     $SUDO sh -c "ln -s /usr/lib/libgtest_main.a /usr/local/lib/googletest/libgtest_main.a"
 fi
 echo "- libgtest is installed in system."
-
-$SUDO sh -c "apt install git-lfs"
-echo "- git-lfs is installed in system."
 
 echo "- Done."
