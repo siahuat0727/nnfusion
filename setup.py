5cf4a190d490d9673cb2744b2649bb73d6801b6c
siahuat0727 <tansiahuat@gmail.com>
2022-02-16 06:27:23 +0000

    Add cmake ext (see comment for warning)
    Failed to support data_files since build doesn't contain
    anything before running setup

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from __future__ import print_function
 
 import os
+import pathlib
 import platform
 import sys
 import tarfile
 
 import setuptools
+from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext as build_ext_orig
 
 if sys.version_info < (3, ):
     print("Please use Python 3 for nnf python library")
@@ -23,9 +26,9 @@ nnf_bin = "build/src/tools/nnfusion/nnfusion"
 nnf_tool = "build/src/tools/nnfusion/"
 nnf_blacklist = ["cmake_install.cmake", "Makefile", "CMakeFiles"]
 
-if not os.path.exists(nnf_bin):
-    print("Pleast build nnfusion in /build folder.")
-    sys.exit(-1)
+# if not os.path.exists(nnf_bin):
+#     print("Pleast build nnfusion in /build folder.")
+#     sys.exit(-1)
 
 
 def get_data_files(source_dir, ignore_list):
@@ -52,6 +55,41 @@ def get_data_files(source_dir, ignore_list):
     ]
 
 
+class CMakeExtension(Extension):
+
+    def __init__(self, name):
+        # don't invoke the original build_ext for this special extension
+        super().__init__(name, sources=[])
+
+
+class build_ext(build_ext_orig):
+
+    def run(self):
+        for ext in self.extensions:
+            self.build_cmake(ext)
+        super().run()
+
+    def build_cmake(self, ext):
+        cwd = pathlib.Path().absolute()
+
+        # these dirs will be created in build_py, so if you don't have
+        # any python sources to bundle, the dirs will be missing
+        build_temp = pathlib.Path(self.build_temp)
+        build_temp = pathlib.Path('build')
+        print(self.build_temp)
+        build_temp.mkdir(parents=True, exist_ok=True)
+        extdir = pathlib.Path(self.get_ext_fullpath(ext.name))
+        extdir.mkdir(parents=True, exist_ok=True)
+
+        os.chdir(str(build_temp))
+        self.spawn(['cmake', str(cwd)])
+        if not self.dry_run:
+            self.spawn(['make', '-j'])
+        # Troubleshooting: if fail on line above then delete all possible
+        # temporary CMake files including "CMakeCache.txt" in top level dir.
+        os.chdir(str(cwd))
+
+
 with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
@@ -76,6 +114,10 @@ setuptools.setup(
     packages=setuptools.find_packages(where="src/python", exclude=["example"]),
     package_dir={"": "src/python"},
     data_files=get_data_files(nnf_tool, nnf_blacklist),
+    ext_modules=[CMakeExtension('.')],
+    cmdclass={
+        'build_ext': build_ext,
+    },
     install_requires=install_requires,
     tests_require=tests_require,
     classifiers=[
